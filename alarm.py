7b91bdb8e87f45960c055d20f5be9fc6f299e316
Alex Henry <tukkek@gmail.com>
2023-06-21 00:00:00 +0000

    First implementation

---
+++ alarm.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+import os,threading,sys,math
+
+BELL='/usr/share/sounds/freedesktop/stereo/complete.oga'
+MINUTE=60
+DURATIONS={'h':60*MINUTE,'m':MINUTE,'s':1,}
+
+message=sys.argv[2] if len(sys.argv)>2 else 'Alarm'
+seconds=-1
+lastalert=False
+
+def sound():
+  if os.path.exists(BELL):
+    os.system('paplay '+BELL)
+
+def alert():
+  global lastalert
+  for d in DURATIONS:
+    if seconds>=DURATIONS[d]:
+      left=round(math.ceil(seconds/DURATIONS[d]))
+      if left>10 and left%10!=0:
+        break
+      text=f'{message} in {left}{d}...'
+      if lastalert!=text:
+        notify(text)
+      lastalert=text
+      break
+
+def notify(text,force=False):
+  print(text)
+  if seconds>=MINUTE or force:
+    os.system(f'notify-send Alarm "{text}"')
+
+def tick():
+  global seconds
+  if seconds>=1:
+    alert()
+    schedule()
+    seconds-=1
+    return
+  notify(message,True)
+  sound()
+
+def parse(argument):
+  argument=argument.lower()
+  for d in DURATIONS:
+    if d in argument:
+      argument=argument.replace(d,'')
+      return int(argument)*DURATIONS[d]
+  return int(argument)
+
+def schedule():
+  threading.Timer(1,tick).start()
+
+seconds=parse(sys.argv[1])
+schedule()
