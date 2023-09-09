cc6eab6258d1341320cd396608919027d8621a4e
Alex Henry <tukkek@gmail.com>
2023-09-09 00:00:00 +0000

    Taking multiple arguments as message

--- alarm.py
+++ alarm.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import os,threading,sys,math
+import os,threading,sys,math,datetime
 
 BELL='/usr/share/sounds/freedesktop/stereo/complete.oga'
 MINUTE=60
 DURATIONS={'h':60*MINUTE,'m':MINUTE,'s':1,}
 
-message=sys.argv[2][0].upper()+sys.argv[2][1:] if len(sys.argv)>2 else 'Alarm'
+message=' '.join(sys.argv[2:]) if len(sys.argv)>2 else 'Alarm'
 seconds=-1
 lastalert=False
 
@@ -13,12 +13,12 @@ def sound():
   if os.path.exists(BELL):
     os.system('paplay '+BELL)
 
-def alert():
+def alert(force=False):
   global lastalert
   for d in DURATIONS:
     if seconds>DURATIONS[d]:
       left=round(math.ceil(seconds/DURATIONS[d]))
-      if left>10 and left%10!=0:
+      if not force and left>10 and left%10!=0:
         break
       text=f'{message} in {left}{d}...'
       if lastalert!=text:
@@ -47,7 +47,16 @@ def parse(argument):
     if d in argument:
       argument=argument.replace(d,'')
       return int(argument)*DURATIONS[d]
-  return int(argument)
+  argument=argument.split(':') if ':' in argument else [argument[0:2],argument[2:4]]
+  argument=[int(a) for a in argument]
+  now=datetime.datetime.now().time()
+  then=datetime.time(argument[0],argument[1])
+  seconds=(then.hour-now.hour)*DURATIONS['h']+(then.minute-now.minute)*DURATIONS['m']
+  if seconds<0:
+    raise Exception('Alarm time in the past!')
+  return seconds
 
 seconds=parse(sys.argv[1])
+message=message[0].upper()+message[1:]
+alert(True)
 tick()
